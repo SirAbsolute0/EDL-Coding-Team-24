53108dcb9e93b554543e4796a004e4eaa39a1499
brianha1222 <68037109+brianha1222@users.noreply.github.com>
2020-07-09 21:06:34 -0500

    Update Deorbit.cpp

--- Deorbit.cpp
+++ Deorbit.cpp
@@ -1,1 +1,19 @@
 #include "Deorbit.h"
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    int x, y, multiplication;
+    
+    cout << "Enter two integers for multiplication: ";
+    cin >> x >> y;
+ 
+    // stored result in third variable
+    multiplication = x * y;
+ 
+    // Prints multiplication value 
+    cout << x << " * " <<  y << " = " << multiplication;     
+ 
+    return 0;
+}
