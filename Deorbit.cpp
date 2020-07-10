94ae0bd6d41a9df277c8aa5db6a25ad36ba5339f
brianha1222 <68037109+brianha1222@users.noreply.github.com>
2020-07-10 13:13:56 -0500

    Update Deorbit.cpp

--- Deorbit.cpp
+++ Deorbit.cpp
@@ -4,16 +4,32 @@ using namespace std;
  
 int main()
 {
-    int x, y, multiplication;
+    int l_sp = 0;
+    float G_0 = 9.806;
+    float v_e;
+    v_e = l_sp * G_0;
+    printf("%3f", v_e);
+ 
+    float G_Const = 0.0000000000667;
+    float M_Mars = 639000000000000000000000.0;
+    float Rad_Mars = 3389500.0;
+    float Delta_V;
     
-    cout << "Enter two integers for multiplication: ";
-    cin >> x >> y;
- 
-    // stored result in third variable
-    multiplication = x * y;
- 
-    // Prints multiplication value 
-    cout << x << " * " <<  y << " = " << multiplication;     
+    Delta_V=sqrt(G_Const*(M_Mars/Alt_0)) * (sqrt((2*Alt_Entry)/(Alt_0 + Alt_Entry))-1);
+    
+    Mass_Fuel=1-pow(e, -Delta_V/v_e);
+    
+    float G_Alt;
+    float Bal_num;
+    float Vel_term;
+    G_Alt= G_Const*M_Mars/ Alt^2;
+    Bal_num= Mass / (Aeroshell_Cd * Aeroshell_A);
+    Vel_term = sqrt ((2*B*G_Alt / Atmo_Dense);
+                     
+    Vel_Sound=sqrt(1.4*287*(Temp+273.15);
+    Mach = Vel/Vel_Sound;
+
+     
  
     return 0;
 }
